3d2e6bde38f25ab7f39b44cf4c30149d111f812b
Daniel Rodriguez <idepixelspa@gmail.com>
2025-06-30 13:52:50 -0400

    Readyy for review

--- public/mail.php
+++ public/mail.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "Fitmas@gmail.com";
+        $recipient = "idepixel@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $subject";
